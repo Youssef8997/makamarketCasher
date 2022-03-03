4271f00cac457c7676f0a59a17d87a7b0a8c25d0
Youssef8997 <67810732+Youssef8997@users.noreply.github.com>
2022-03-03 16:47:46 +0200

    y

--- lib/models/Casherpage.dart
+++ lib/models/Casherpage.dart
@@ -3,14 +3,48 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled6/cuibt/cuibt.dart';
 
 import '../cuibt/State.dart';
+
 class CasherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<CasherCuibt,CasherState>
-      (listener: (context,state){},
-        builder: (context,state){
-          return Container();
-        }
+    return BlocConsumer<CasherCuibt, CasherState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          var size=MediaQuery.of(context).size;
+          return Column(
+              children: [
+          NumberOfOrder(),
+           Align(
+             alignment: AlignmentDirectional.center,
+             child: Container(
+               color: Colors.grey[300],
+               height: size.height*.8,
+               width: size.width*.9,
+               child: Ta,
+
+             ),
+           )
+
+          ]);
+        });
+  }
+
+  Align NumberOfOrder() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+            height: 40,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(20)
+            ),
+alignment: AlignmentDirectional.center,
+child: SelectableText("1000",style: TextStyle(fontWeight: FontWeight.bold),)
+        ),
+      ),
     );
   }
 }
